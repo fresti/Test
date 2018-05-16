81f257c4d470019c324d02bf3127805e1df32452
fresti <33902893+fresti@users.noreply.github.com>
2018-05-16 16:03:59 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int a = 1, b = 2, c = 4, x = 0;
+
+  while(1){
+    printf("Bienvenue dans le programme de test");
+    
+    x = a*b;
+    printf("Voici le résultat de la multiplication : %d\n", x);
+    
+    x = 0;
+    x = c / b;
+    printf("Voici le résultat de la division : %d\n", x);
+    
+    x = 0;
+    x = a + b + c;
+    printf("Voici le résultat de l'addition : %d\n", x);
+    
+    x = 0;
+    }
+}
+    
