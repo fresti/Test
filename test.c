5057c89d013b37868cd706727142c20f8d0170a6
student <thibault.prieur@ucare-watches.com>
2018-05-16 16:44:41 +0200

    Test

--- test.c
+++ test.c
@@ -3,6 +3,8 @@
 
 int main (void){
   printf("Test ne sers à rien\n");
+  printf("Test sers à quelque chose\n");
+  printf("Bien joué tu as reussi à up ton projet sur GitHub\n");
   
   return 0;
   
