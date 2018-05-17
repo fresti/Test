347546b9190cb5ab48b0804b63f1bd93333342ad
fresti <33902893+fresti@users.noreply.github.com>
2018-05-17 10:56:35 +0200

    Update test.c

--- test.c
+++ test.c
@@ -5,6 +5,7 @@ int main (void){
   printf("Test ne sers à rien\n");
   printf("Test sers à quelque chose\n");
   printf("Bien joué tu as reussi à up ton projet sur GitHub\n");
+  printf("Bien joué tu as réusiss à récuperer les modifs sous MCUxpresso");
   
   return 0;
   
